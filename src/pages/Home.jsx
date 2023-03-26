d994f32569c8cc66a384821bd2ff4b19df061c41
Gerardo Cadau <gerardocadau@gmail.com>
2023-03-26 11:30:00 -0300

    change favicon and clean code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
-import Header from '../partials/Header';
 import PageIllustration from '../partials/PageIllustration';
 import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
 import FeaturesZigZag from '../partials/FeaturesZigzag';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Banner from '../partials/Banner';
 import Footer from '../partials/Footer';
 
 function Home() {
@@ -21,14 +16,10 @@ function Home() {
         <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
           <PageIllustration />
         </div>
-
         {/*  Page sections */}
         <HeroHome />
         <FeaturesZigZag />
       </main>
-
-      <Banner />
-
       {/*  Site footer */}
       <Footer />
     </div>
